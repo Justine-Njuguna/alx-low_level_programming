aab1886d4ffe1977a0bffb116cbc09d3944eae38
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-02-16 05:47:28 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,19 @@
-/** main entry point
-*
-* Return Always 0(success)
-*/
+/*
+ * File: 5-printf.c
+ * Auth: Justine Njuguna
+ */
+
 #include <stdio.h>
 
-int main(void) 
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
