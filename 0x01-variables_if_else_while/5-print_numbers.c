ae826ee01abf3212645bf9944a22b6ea83eff1f1
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-02-17 05:30:01 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,31 +1,18 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: Justine Njuguna
- */
-
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
- *
- * Return: Always 0.
+* main - Entry point
+* printf to print whole numbers
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	int n;
+	char c = '0';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
 	return (0);
 }
