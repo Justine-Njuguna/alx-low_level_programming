e7bff12a3f48355392eebbf41f5384f88ed33ee2
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-02-17 05:34:52 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Description: Prints all single digit numbers of base 10 starting from 0
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* putchar to print combinations of sing dig numbers
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-        int num;
+	int i = '0';
 
-        for (num = 0; num < 10; num++)
-                putchar(num + '0');
-
-        putchar('\n');
-
-        return (0);
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i < '9')
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		i++;
+	}
+	putchar(10);
+	return (0);
 }
