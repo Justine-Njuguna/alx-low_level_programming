69e208258ab13b7469a6ffa9aa3cef2cd49f1e68
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-04-10 13:03:52 -0700

    Tread lighty, she is near!

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: Main.h
+ * Author: Justine Njuguna
+ * Desc: Header file that contains prototypes for all funtions written
+ *		in the file i/o directory.
+ */
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* Ending my MAIN_H */
