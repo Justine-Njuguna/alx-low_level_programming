c3902cddc2ff803a6f08322a214e00f395c478b0
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-08-03 08:46:26 -0700

    Added main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Custom _putchar function */
+int _putchar(char c);
+
+/* Function to convert a binary string to an unsigned int - Task 1 */
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
