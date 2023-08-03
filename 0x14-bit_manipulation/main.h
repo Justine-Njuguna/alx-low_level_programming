5ed2529cc5683d1a90e1d407a915c24700d45543
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-08-03 10:56:10 -0700

    Task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,11 +4,15 @@
 /* Our .h files */
 #include <stddef.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /* Custom _putchar function */
 int _putchar(char c);
 
-/* Function to convert a binary string to an unsigned int - Task 1 */
+/* Function to convert a binary string to an unsigned int - Task 0 */
 unsigned int binary_to_uint(const char *b);
+
+/* Function that prints the binary representation of a number Task 1 */
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
