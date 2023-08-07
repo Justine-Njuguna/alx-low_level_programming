e91d59be885030e35a2e3f6d2b5a55b0cbf62019
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-08-07 12:38:18 -0700

    Added a README

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* All header files */
+/* =============================== */
+
+/* All Prototypes */
+/* ================================ */
+
+/* Prototype for _putchar */
+int _putchar(char c);
+
+
+#endif /* End of the main.h */
