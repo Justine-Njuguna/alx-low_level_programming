de19c647913fa5eaf02259eecfc888d98b5aa86c
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-08-07 13:21:22 -0700

    Task 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,7 +15,10 @@
 /* Prototype for _putchar */
 int _putchar(char c);
 
-/* Function that reads a text file and prints it to the POSIX STD_OUT */
+/* Function that reads a text file and prints it to the POSIX STD_OUT - Task 0 */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Function that creates a file - Task 1 */
+int create_file(const char *filename, char *text_content);
 
 #endif /* End of the main.h */
