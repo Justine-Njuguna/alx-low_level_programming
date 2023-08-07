72cf8bad1cc743a726fde10a47d246261a667f7d
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-08-07 13:25:18 -0700

    Updated Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,9 +5,8 @@
  * @filename: The name of the file to create.
  * @text_content: The text content to write to the file.
  *
- * Return: 1 on success, -1 on fail.
+ * Return: 1 on success, -1 on failure.
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd, bytes_written = 0, length = 0;
@@ -21,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd  == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
@@ -36,6 +35,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
-
-	return (-1);
+	return (1);
 }
+
