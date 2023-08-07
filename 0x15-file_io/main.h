4f04fb62a1771adf70ec007ed5753a2af9502882
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-08-07 13:06:19 -0700

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,11 +4,18 @@
 /* All header files */
 /* =============================== */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /* All Prototypes */
 /* ================================ */
 
 /* Prototype for _putchar */
 int _putchar(char c);
 
+/* Function that reads a text file and prints it to the POSIX STD_OUT */
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* End of the main.h */
