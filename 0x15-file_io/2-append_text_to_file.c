46f2974d83e0967c7c86ae11abdb17eb04c27912
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-08-07 13:37:42 -0700

    Task 3

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * append-text_to_file - Appends text to EOF.
+ * @filename: The name of the file.
+ * @text_content: Text content to append to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written = 0, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[length])
+			length++;
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, length);
+		if (bytes_written == -1 || bytes_written != length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -21,4 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* Function that creates a file - Task 1 */
 int create_file(const char *filename, char *text_content);
 
+/* Function that appends text at the end of a file - Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif /* End of the main.h */
