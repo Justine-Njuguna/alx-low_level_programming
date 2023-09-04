4662920d4f7c6c795154aa67062cabc6649d9b19
Justine Njuguna <hdenlysjustine@gmail.com>
2023-09-04 15:29:08 +0300

    Icite

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,112 +1,96 @@
 #include "main.h"
 
 /**
- * custom_check97 - Check for the correct number of arguments.
- * @argc: Num of args
+ * allocate_buffer - Allocate memory for a buffer.
+ * @file: The filename
  *
- * Return: void.
+ * Return: A ptr to the allocated buffer.
  */
 
-void custom_check97(int argc)
+char *allocate_buffer(char *file)
 {
-	if (argc != 3)
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Custom Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO,
+				"Custom Error: Unable to allocate memory for %s\n", file);
+		exit(99);
 	}
+
+	return (buffer);
 }
 
 /**
- * custom_check_read - Check if file_from exists and can be read.
- * @check: Check if true or false
- * @file: file_from name
- * @fd_from: File descriptor of file_from, or -1
- *
- * Return: void.
+ * close_descriptor - Close a fd
+ * @fd: The fd to be closed
  */
 
-void custom_check_read(ssize_t check, char *file, int fd_from)
+void close_descriptor(int fd)
 {
-	if (check == -1)
+	int result;
+
+	result = close(fd);
+
+	if (result == -1)
 	{
-		dprintf(STDERR_FILENO, "Custom Error: Can't read from file %s\n", file);
-		if (fd_from != -1)
-			close(fd_from);
-		exit(98);
-	}
-}
-
-/**
- * custom_check_write - Check if file_to was created and/or can be written to.
- * @check: Check if true or false.
- * @file: file_to name
- * @fd_to: File descriptor of file_to, or -1
- *
- * Return: void
- */
-
-void custom_check_write(ssize_t check, char *file, int fd_to)
-{
-	if (check == -1)
-	{
-		dprintf(STDERR_FILENO, "Custom Error: Can't write to %s\n", file);
-		if (fd_to != -1)
-			close(fd_to);
-		exit(98);
-	}
-}
-
-/**
- * custom_check_close - Check that file descriptor was closed properly.
- * @check: Check if true or false
- * @fd: File descriptor
- *
- * Return: void
- */
-
-void custom_check_close(int check, int fd)
-{
-	if (check == -1)
-	{
-		dprintf(STDERR_FILENO, "Custom Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Custom Error: Unable to close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
  * main - Entry point
- * @argc: Number of arguments passed
- * @argv: Array of pointers to the arguments
+ * @argc: The number of command-line arguments.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0
+ * Return: 0 on success.
  */
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, close_to, close_from;
-	ssize_t lenr, lenw;
-	char buffer[1024];
-	mode_t file_perm;
+	int source_fd, destination_fd, read_bytes, write_bytes;
+	char *buffer;
 
-	custom_check97(argc);
-	fd_from = open(argv[1], O_RDONLY);
-	custom_check_read((ssize_t)fd_from, argv[1], -1);
-	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	custom_check_write((ssize_t)fd_to, argv[2], fd_from);
-	lenr = 1024;
-	while (lenr == 1024)
+	if (argc != 3)
 	{
-		lenr = read(fd_from, buffer, 1024);
-		custom_check_read(lenr, argv[1], fd_from);
-		lenw = write(fd_to, buffer, lenr);
-		if (lenw != lenr)
-			lenw = -1;
-		custom_check_write(lenw, argv[2], fd_to);
+		dprintf(STDERR_FILENO, "Custom Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	close_to = close(fd_to);
-	close_from = close(fd_from);
-	custom_check_close(close_to, fd_to);
-	custom_check_close(close_from, fd_from);
+
+	buffer = allocate_buffer(argv[2]);
+	source_fd = open(argv[1], O_RDONLY);
+	read_bytes = read(source_fd, buffer, 1024);
+	destination_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (source_fd == -1 || read_bytes == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Custom Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		write_bytes = write(destination_fd, buffer, read_bytes);
+		if (destination_fd == -1 || write_bytes == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Custom Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		read_bytes = read(source_fd, buffer, 1024);
+		destination_fd = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (read_bytes > 0);
+
+	free(buffer);
+	close_descriptor(source_fd);
+	close_descriptor(destination_fd);
+
 	return (0);
 }
