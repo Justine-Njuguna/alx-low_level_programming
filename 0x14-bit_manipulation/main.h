357c8b893192031b01bd9ce13ca3723324e3b4e1
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-09-28 05:05:59 -0700

    Added main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* putchar */
+int _putchar(char c);
+
+/* Function prototypes */
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
