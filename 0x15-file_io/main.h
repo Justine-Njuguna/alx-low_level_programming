84fda88336e14dfa981b5e11af0d4df1467d235d
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-10-02 07:49:32 -0700

    Task 0 complete

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,8 +11,35 @@
 #include <elf.h>
 #define BUFFER_SIZE 1024
 
+
 /* Task 0 */
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/* Task 1 */
+int create_file(const char *filename, char *text_content);
 
-#endif
+/* Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* Task 3 */
+void handle_error(int exit_code, const char *message);
+
+/* Task 3 */
+void copy_file(const char *file_from, const char *file_to);
+
+/* Task 4 */
+void display_elf_header(Elf64_Ehdr *header);
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+#endif /* MAIN_H */
+
