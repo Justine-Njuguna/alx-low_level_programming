0e952159a08222bbb28c217bcb0e5185d68e222b
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-10-02 03:52:21 -0700

    Betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,14 +13,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	ssize_t bytes_read, bytes_written;
 	char *buffer;
-
+	
 	if (filename == NULL)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
@@ -28,15 +26,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1 || bytes_read != bytes_written)
+	if (bytes_read == -1)
 	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
+                free(buffer);
+                close(fd);
+                return (0);
+        }
+        bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+        if (bytes_written == -1 || bytes_written != bytes_read)
+        {
+                free(buffer);
+                close(fd);
+                return (0);
+        }
 	free(buffer);
 	close(fd);
-
 	return (bytes_written);
 }
