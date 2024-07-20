62e5da4be46ba8a2fc480c25b52fa14d8cabc58a
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-07-20 21:38:56 +0300

    Task-00

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+
+/* Prototypes for all the files */
+
+/* Task-0 */
+int linear_search(int *array, size_t size, int value);
+
+
+#endif /* SEARCH_ALGOS_H */
